ccfab6c775ee76aeafbe15277e06763bd52b1785
fwjab <jahbaristewart@gmail.com>
2026-07-20 10:57:37 -0400

    Create player.js

---
+++ player.js
@@ -0,0 +1,53 @@
+const player={
+
+level:1,
+
+rank:"E",
+
+xp:0,
+
+gold:0,
+
+statPoints:0,
+
+title:"Rookie Hunter",
+
+equippedShadow:null,
+
+equippedTheme:"Blue Hunter",
+
+inventory:[],
+
+titles:["Rookie Hunter"],
+
+shadows:[],
+
+themes:["Blue Hunter"],
+
+achievements:[],
+
+upgrades:{
+
+xpBoost:0,
+
+extraDailyQuest:false,
+
+bossChance:0,
+
+inventorySlots:20
+
+},
+
+stats:{
+
+str:10,
+
+agi:10,
+
+end:10,
+
+vit:10
+
+}
+
+};
