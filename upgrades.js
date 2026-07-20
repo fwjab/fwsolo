e757300f365986591e8ed2d2765bb883307afc13
fwjab <jahbaristewart@gmail.com>
2026-07-20 11:03:47 -0400

    Create upgrades.js

---
+++ upgrades.js
@@ -0,0 +1,38 @@
+const upgrades=[
+
+{
+
+name:"XP Gain +5%",
+
+cost:10000
+
+},
+
+{
+
+name:"Second Daily Quest",
+
+cost:20000
+
+},
+
+{
+
+name:"Boss Spawn +10%",
+
+cost:25000
+
+},
+
+{
+
+name:"Inventory +10",
+
+cost:5000
+
+}
+
+];
+function buyUpgrade(){
+
+}
