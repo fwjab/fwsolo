03da409548de036558ca39d60a878d92ffcb6972
fwjab <jahbaristewart@gmail.com>
2026-07-20 11:02:29 -0400

    Create shadows.js

---
+++ shadows.js
@@ -0,0 +1,41 @@
+const shadows=[
+
+{
+
+name:"Iron Knight",
+
+cost:7000
+
+},
+
+{
+
+name:"Shadow Wolf",
+
+cost:10000
+
+},
+
+{
+
+name:"Shadow Mage",
+
+cost:15000
+
+},
+
+{
+
+name:"Shadow Dragon",
+
+cost:50000
+
+}
+
+];
+function summonShadow(){
+
+}
+function equipShadow(){
+
+}
