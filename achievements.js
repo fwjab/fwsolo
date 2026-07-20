7d24d8eb976b508a06403ee5ffce1e658bd25d39
fwjab <jahbaristewart@gmail.com>
2026-07-20 11:00:48 -0400

    Create achievements.js

---
+++ achievements.js
@@ -0,0 +1,37 @@
+const achievements=[
+
+{
+
+id:"quest10",
+
+name:"First Steps",
+
+goal:10,
+
+rewardGold:500,
+
+rewardXP:500
+
+},
+
+{
+
+id:"quest100",
+
+name:"Dungeon Veteran",
+
+goal:100,
+
+rewardGold:5000,
+
+rewardXP:3000
+
+}
+
+];
+function checkAchievements(){
+
+}
+function unlockAchievement(){
+
+}
