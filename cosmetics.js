4e2813ae46db2a53a9645bdf90309d37a723ef79
fwjab <jahbaristewart@gmail.com>
2026-07-20 11:03:03 -0400

    Create cosmetics.js

---
+++ cosmetics.js
@@ -0,0 +1,38 @@
+const cosmetics=[
+
+{
+
+name:"Blue Hunter",
+
+price:0
+
+},
+
+{
+
+name:"Purple Hunter",
+
+price:6000
+
+},
+
+{
+
+name:"Red Monarch",
+
+price:25000
+
+},
+
+{
+
+name:"Golden King",
+
+price:50000
+
+}
+
+];
+function equipTheme(){
+
+}
