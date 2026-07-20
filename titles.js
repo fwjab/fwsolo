42b6f2472337d949e4f71f196fedf623abdeb0e9
fwjab <jahbaristewart@gmail.com>
2026-07-20 11:01:44 -0400

    Create titles.js

---
+++ titles.js
@@ -0,0 +1,38 @@
+const titles=[
+
+{
+
+name:"Rookie Hunter",
+
+unlock:"Default"
+
+},
+
+{
+
+name:"Dungeon Explorer",
+
+unlock:"Complete 100 Quests"
+
+},
+
+{
+
+name:"Elite Hunter",
+
+unlock:"Reach Rank B"
+
+},
+
+{
+
+name:"Shadow Monarch",
+
+unlock:"Reach Rank S"
+
+}
+
+];
+function equipTitle(){
+
+}
