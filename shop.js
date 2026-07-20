ff2579d29a888c03fa4c34193f604797edbe313e
fwjab <jahbaristewart@gmail.com>
2026-07-20 10:58:34 -0400

    Create shop.js

---
+++ shop.js
@@ -0,0 +1,51 @@
+const shopItems=[
+
+{
+
+id:1,
+
+name:"Blue Hunter Theme",
+
+price:2500,
+
+type:"theme"
+
+},
+
+{
+
+id:2,
+
+name:"Shadow Wolf",
+
+price:8000,
+
+type:"shadow"
+
+},
+
+{
+
+id:3,
+
+name:"Quest Refresh",
+
+price:500,
+
+type:"item"
+
+},
+
+{
+
+id:4,
+
+name:"XP Boost",
+
+price:2500,
+
+type:"upgrade"
+
+}
+
+];
